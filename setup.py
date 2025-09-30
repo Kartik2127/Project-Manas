c5c17ba2e465fa8581b46bed3c909cde55fb70f4
Kartik2127 <thakurkartik9612@gmail.com>
2025-09-30 14:16:36 +0530

    build: Add basic setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name='ProjectManas',
+    version='0.0.1',
+    author='Kartik, Tushar and Nikhil',
+    author_email='your_email@example.com',
+    packages=find_packages(),
+    install_requires=[],
+)
